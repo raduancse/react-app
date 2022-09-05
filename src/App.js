ab8a59058927b36328972434e59c4b7f3fe0621f
Md. Raduan Islam <raduan.cse@gmail.com>
2022-09-05 18:21:24 +0600

    react router dom install

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {Routes, Route, Link} from 'react-router-dom';
+import News from './pages/News';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div className="topnav" id="myTopnav">
+          <Link to='/' className="active">
+            Home
+          </Link>
+          <Link to='/news'>News</Link>
+        </div>
       </header>
+
+      <Routes>
+        <Route path="/news" element={<News/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/News.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const News = () => {
+    return (
+        <div>
+            Hello News Component
+        </div>
+    );
+}
+
+export default News;
